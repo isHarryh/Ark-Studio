b540eafde60b8b0f4227d9aff2ef52b1141dd575
Harry Huang <harryhuang2652@qq.com>
2025-06-07 02:03:03 +0800

    feat: sync lz4ak compression from ArkUnpacker

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2024-2025, Harry Huang
 # @ BSD 3-Clause License
+
+
+def __add_lz4ak_decompression():
+    from UnityPy.helpers import CompressionHelper
+    from UnityPy.enums.BundleFile import CompressionFlags
+    from .Block import decompress_lz4ak
+
+    # New compression algorithm introduced in Arknights v2.5.04+
+    CompressionHelper.DECOMPRESSION_MAP[CompressionFlags.LZHAM] = decompress_lz4ak
+
+
+__add_lz4ak_decompression()
